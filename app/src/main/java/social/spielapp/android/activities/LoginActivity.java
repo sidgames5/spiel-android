a1314034521b4e1b04308c6bfbb9e57ddada0336
SidGames5 <saytleader@outlook.com>
2023-11-07 10:43:33 -0500

    add some functionality

--- app/src/main/java/social/spielapp/android/activities/LoginActivity.java
+++ app/src/main/java/social/spielapp/android/activities/LoginActivity.java
@@ -2,7 +2,9 @@ package social.spielapp.android.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import social.spielapp.android.R;
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void gotoRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
