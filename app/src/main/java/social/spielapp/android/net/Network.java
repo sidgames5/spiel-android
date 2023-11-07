990abf3f472f753e413e5b1d46468e80ade0a29c
SidGames5 <saytleader@outlook.com>
2023-11-07 09:48:42 -0500

    network stuff

---
+++ app/src/main/java/social/spielapp/android/net/Network.java
@@ -0,0 +1,25 @@
+package social.spielapp.android.net;
+
+import kotlin.NotImplementedError;
+
+public class Network {
+    public static void sendMessage(String message) {
+        throw new NotImplementedError();
+    }
+
+    public static void onMessageReceive(String message) {
+        throw new NotImplementedError();
+    }
+
+    public static void authenticate(String username, String password) {
+        throw new NotImplementedError();
+    }
+
+    public static void onAuthSuccess(String token) {
+        throw new NotImplementedError();
+    }
+
+    public static void onAuthError(String error) {
+        throw new NotImplementedError();
+    }
+}
