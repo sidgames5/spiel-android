b33de37301afa8a955c6c1bd3a270413eb3c3d73
SidGames5 <saytleader@outlook.com>
2023-11-07 10:36:41 -0500

    websocket stuff

--- app/src/main/java/social/spielapp/android/net/Network.java
+++ app/src/main/java/social/spielapp/android/net/Network.java
@@ -4,6 +4,8 @@ import kotlin.NotImplementedError;
 import okhttp3.*;
 
 public class Network {
+    private static WebSocket webSocket;
+
     public static void sendMessage(String message) {
         throw new NotImplementedError();
     }
@@ -22,5 +24,13 @@ public class Network {
 
     public static void onAuthError(String error) {
         throw new NotImplementedError();
+    }
+
+    public static void createWebsocket() {
+        throw new NotImplementedError();
+    }
+
+    public static WebSocket getWebSocket() {
+        return webSocket;
     }
 }
