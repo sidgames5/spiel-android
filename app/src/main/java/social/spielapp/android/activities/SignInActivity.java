2949e468919d53ac66e23c99f82ae49673fb3f3c
SidGames5 <saytleader@outlook.com>
2023-11-07 08:53:08 -0500

    basic stuff

---
+++ app/src/main/java/social/spielapp/android/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package social.spielapp.android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import social.spielapp.android.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
